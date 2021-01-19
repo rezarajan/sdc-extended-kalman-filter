6e88c650e3313cca02434a224a250b84b30962f2
rezarajan <rezarajan@gmail.com>
2021-01-18 20:09:21 -0400

    feat: radar measurement space to state space map

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -78,11 +78,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack)
     {
       // TODO: Convert radar from polar to cartesian coordinates
       //         and initialize state.
-      // TODO: Update this for radar measurements
-      ekf_.x_ << measurement_pack.raw_measurements_[0],
-          measurement_pack.raw_measurements_[1],
-          0,
-          0;
+      // px, py, vx, vy
+      ekf_.x_(0) = measurement_pack.raw_measurements_[0]*cos(measurement_pack.raw_measurements_[1]);
+      ekf_.x_(1) = measurement_pack.raw_measurements_[0]*sin(measurement_pack.raw_measurements_[1]);
+      ekf_.x_(2) = 0;
+      ekf_.x_(3) = 0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER)
     {
